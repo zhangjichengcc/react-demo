5bac5bb62f8092a227317ed2c3be58e38106f835
zhangjicheng <zhangjichengcc@163.com>
2023-08-21 18:19:21 +0800

    add useImperativeHandle

--- config/routes.config.tsx
+++ config/routes.config.tsx
@@ -2,23 +2,24 @@
  * @Author: zhangjicheng
  * @Date: 2022-07-27 14:17:25
  * @LastEditors: zhangjicheng
- * @LastEditTime: 2022-10-11 18:55:58
- * @FilePath: \react-demo\config\routes.config.tsx
+ * @LastEditTime: 2023-08-21 18:01:27
+ * @FilePath: /react-demo/config/routes.config.tsx
  */
-import { lazy } from 'react';
-import { RouteObject } from 'react-router';
-import lazyLoad from '@utils/lazyLoad';
+import { lazy } from "react";
+import { RouteObject } from "react-router";
+import lazyLoad from "@utils/lazyLoad";
 
-import BlankLayout from '@/layouts/BlankLayout';
-import BaseLayout from '@/layouts/BaseLayout';
+import BlankLayout from "@/layouts/BlankLayout";
+import BaseLayout from "@/layouts/BaseLayout";
 
-const Index = lazy(() => import('@/pages/Index'));
-const Home = lazy(() => import('@/pages/Home'));
-const UseReducer = lazy(() => import('@/pages/UseReducer'));
-const UseContext = lazy(() => import('@/pages/UseContext'));
-const UseSyncExternalStore = lazy(() => import('@/pages/UseSyncExternalStore'));
-const UseMemo = lazy(() => import('@/pages/UseMemo'));
-const ReactQuery = lazy(() => import('@/pages/ReactQuery'));
+const Index = lazy(() => import("@/pages"));
+const Home = lazy(() => import("@/pages/Home"));
+const UseReducer = lazy(() => import("@/pages/UseReducer"));
+const UseContext = lazy(() => import("@/pages/UseContext"));
+const UseSyncExternalStore = lazy(() => import("@/pages/UseSyncExternalStore"));
+const UseMemo = lazy(() => import("@/pages/UseMemo"));
+const UseImperativeHandle = lazy(() => import("@/pages/UseImperativeHandle"));
+const ReactQuery = lazy(() => import("@/pages/ReactQuery"));
 
 const routes: RouteObject[] = [
   // {
@@ -32,38 +33,42 @@ const routes: RouteObject[] = [
   //   ]
   // },
   {
-    path: '/',
+    path: "/",
     element: <BaseLayout />,
     children: [
       {
-        path: '',
+        path: "",
         element: lazyLoad(<Index />),
       },
       {
-        path: 'useReducer',
+        path: "useReducer",
         element: lazyLoad(<UseReducer />),
       },
       {
-        path: 'useContext',
+        path: "useContext",
         element: lazyLoad(<UseContext />),
       },
       {
-        path: 'useSyncExternalStore',
+        path: "useSyncExternalStore",
         element: lazyLoad(<UseSyncExternalStore />),
       },
       {
-        path: 'useMemo',
+        path: "useMemo",
         element: lazyLoad(<UseMemo />),
       },
       {
-        path: 'reactQuery',
+        path: "reactQuery",
         element: lazyLoad(<ReactQuery />),
       },
       {
-        path: '/home',
+        path: "useImperativeHandle",
+        element: lazyLoad(<UseImperativeHandle />),
+      },
+      {
+        path: "/home",
         element: <Home />,
-      }
-    ]
+      },
+    ],
   },
 ];
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@
  * @Author: zhangjicheng
  * @Date: 2022-07-25 17:04:42
  * @LastEditors: zhangjicheng
- * @LastEditTime: 2023-08-07 10:03:19
+ * @LastEditTime: 2023-08-21 18:16:35
  * @FilePath: /react-demo/src/pages/index.tsx
  */
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import PageHead from "@/components/PageHead";
+import { Route, Link, Routes, useLocation, useRoutes } from "react-router-dom";
 // import { Button, PopOver } from 'react-plugin';
 // import PopOver from '@/components/PopOver'
 import { NavLink } from "react-router-dom";
@@ -18,33 +19,15 @@ const Index: FC = () => {
   return (
     <div className={styles.view}>
       <PageHead>React hooks</PageHead>
-      <div style={{ height: 40, overflow: "hidden" }}>
-        <ScrollWrapper direction="tb">
-          <div className={styles.nav}>
-            <NavLink to="/home">1. 首页</NavLink>
-            <NavLink to="/useReducer">2. useReducer</NavLink>
-            <NavLink to="/useContext">3. useContext</NavLink>
-            <NavLink to="/useSyncExternalStore">
-              4. useSyncExternalStore
-            </NavLink>
-            <NavLink to="/useMemo">5. useMemo</NavLink>
-            <p>plugin</p>
-            <NavLink to="/reactQuery">react-query</NavLink>
-            {/* <Button>Button</Button>
-          <PopOver /> */}
-          </div>
-        </ScrollWrapper>
-      </div>
       <div className={styles.nav}>
         <NavLink to="/home">1. 首页</NavLink>
         <NavLink to="/useReducer">2. useReducer</NavLink>
         <NavLink to="/useContext">3. useContext</NavLink>
         <NavLink to="/useSyncExternalStore">4. useSyncExternalStore</NavLink>
         <NavLink to="/useMemo">5. useMemo</NavLink>
+        <NavLink to="/useImperativeHandle">6. useImperativeHandle</NavLink>
         <p>plugin</p>
         <NavLink to="/reactQuery">react-query</NavLink>
-        {/* <Button>Button</Button>
-      <PopOver /> */}
       </div>
     </div>
   );
