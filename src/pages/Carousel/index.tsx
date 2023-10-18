5ba1a15ed36a3211f604e4fbcbfe04e2d649370c
zhangjicheng <zhangjichengcc@163.com>
2023-10-18 15:37:56 +0800

    update carousel

--- src/pages/Carousel/index.tsx
+++ src/pages/Carousel/index.tsx
@@ -2,10 +2,22 @@ import { FC } from "react";
 
 import styles from "index.less";
 
+const Page: FC<{ num: number }> = (props) => {
+  const { num } = props;
+
+  return (
+    <div>
+      <h1>Page {num}</h1>
+    </div>
+  );
+};
+
 const Carousel: FC = () => {
   return (
     <div>
-      <h1>Carousel</h1>
+      <Page num={1} />
+      <Page num={2} />
+      <Page num={3} />
     </div>
   );
 };
